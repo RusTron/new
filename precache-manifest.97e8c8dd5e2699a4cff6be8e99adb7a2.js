08ac6d33abbc8e161adb32f6fefc613a708bea1a
RusTron <rusan.tron@gmail.com>
2020-10-06 19:51:30 +0300

    Updates

---
+++ precache-manifest.97e8c8dd5e2699a4cff6be8e99adb7a2.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0a236df85f67235c4e2ca5bd40e5fe",
+    "url": "/new/index.html"
+  },
+  {
+    "revision": "af4b5fff1388e210a418",
+    "url": "/new/static/css/2.cb65a257.chunk.css"
+  },
+  {
+    "revision": "d445104037f00d74e240",
+    "url": "/new/static/css/main.d2462b1b.chunk.css"
+  },
+  {
+    "revision": "af4b5fff1388e210a418",
+    "url": "/new/static/js/2.45fe5c87.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/new/static/js/2.45fe5c87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d445104037f00d74e240",
+    "url": "/new/static/js/main.105488ab.chunk.js"
+  },
+  {
+    "revision": "f440cf0a1a0f77f48435",
+    "url": "/new/static/js/runtime-main.73b2fe32.js"
+  },
+  {
+    "revision": "f7eab148df329432a159f8a1cccb102f",
+    "url": "/new/static/media/backdrop.f7eab148.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "324b96ae75a3810e840babcae038bea4",
+    "url": "/new/static/media/wezom-logo.324b96ae.svg"
+  }
+]);
